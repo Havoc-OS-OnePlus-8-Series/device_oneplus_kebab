829b7a705325ee17405cab22797b2caa6a53de69
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-03-01 13:22:06 +0800

    kebab: Set default hotspot ssid by RRO
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -26,5 +26,9 @@
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Wifi Overlay
+PRODUCT_PACKAGES += \
+    OnePlus8TWifiOverlay
+
 # Inherit from oneplus sm8250-common
 $(call inherit-product, device/oneplus/sm8250-common/common.mk)
